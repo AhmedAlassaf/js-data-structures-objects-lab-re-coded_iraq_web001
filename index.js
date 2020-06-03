2302fa4c7e086d50184db4ac19169213401d37d4
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-06-03 14:00:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
   return driver;
 }
+
+function deleteFromDriverByKey(driver, key){
+  const newDriver = { ...driver };
+  newDriver[key] = delete[key];
+  return newDriver;
+}
