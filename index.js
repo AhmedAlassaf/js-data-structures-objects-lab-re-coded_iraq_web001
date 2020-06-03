98d40fdc7094c5c25a8078685201edb23d616d33
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-06-03 14:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ function deleteFromDriverByKey(driver, key){
   return newDriver;
 }
 
-function deleteFromDriverByKey(driver, key){
-  const newDriver = { ...driver };
-  delete newDriver.key;
-  return newDriver;
+function destructivelyDeleteFromDriverByKey(driver, key){
+  delete driver.key;
+  return driver;
 }
